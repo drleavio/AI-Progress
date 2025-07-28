60a7cfdebb348905e8c8de5f5d8453d799650d88
drleavio <drlevio123@gmail.com>
2025-07-28 13:58:41 +0530

    learning python syntax

---
+++ pythonBasics/firstDay.py
@@ -0,0 +1,22 @@
+# import sys
+
+# print(sys.version)
+# print("hello world")
+
+# x=10
+# y=20
+
+# def add(x,y):
+#     print(x+y)
+
+# add(x,y)
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
